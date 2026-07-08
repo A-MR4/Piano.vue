abe8ebe0a9e5766866b31563b7b871adc6cfa2ef
Alejandro Montiel <alejandro.montiel4554@alumnos.udg.mx>
2026-07-08 13:05:15 -0600

    Correciones del config

--- piano-browser/vite.config.js
+++ piano-browser/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Piano.vue/'
+  base: '/Piano.vue/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
